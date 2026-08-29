68345ba0ce3f6fdf36eb0906cf4448a3f984592a
LawrenceHollom <lawrence@hollom.com>
2026-08-29 07:24:30 +0200

    Some transition graphics

--- scenes/toplevel/transition.gd
+++ scenes/toplevel/transition.gd
@@ -1,10 +1,17 @@
 extends Control
 
+@export var colour_rect: ColorRect
+@export var texture_rect: TextureRect
+@export var label: Label
+
+@export var eyes: Array[Texture2D]
+@export var babel: Texture2D
+@export var tree: Texture2D
+@export var flower: Texture2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var timer: SceneTreeTimer = get_tree().create_timer(2.0)
-	timer.timeout.connect(_on_transition_finished)
+	var tween = create_tween()
 
 
 func _on_transition_finished() -> void:
